0b9b92eb95697ca00c146843ac233fa13e1785e3
Sharat Chandra <sharat9211@gmail.com>
2021-05-19 11:52:16 +0530

    Add github action code

--- day-5/server.ts
+++ day-5/server.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { fetchData, URL } from '../day-6/http-client'
 import App, { StandardError } from './framework'
 import { TodoController } from './todo'
@@ -17,5 +18,10 @@ app.get('/outgoing', async () => fetchData(URL))
 
 app.get('/todos', TodoController.getInstance().getList)
 app.post('/todos', TodoController.getInstance().createItem)
+
+app.get('/upstream', async () => {
+    const res = await axios.get('http://upstream:8080')
+    return res.data
+})
 
 export default app.start()
